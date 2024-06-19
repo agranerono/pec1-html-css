27367e39cb0044e70e7a63cc0cb832e49f34f413
Alejandro Granero Nogales <agranerono@uoc.edu>
2024-06-20 01:57:46 +0200

    test video

--- src/scripts/video.js
+++ src/scripts/video.js
@@ -1,4 +1,21 @@
 function loadYoutubeVideo() {
     var container = document.querySelector('.youtube-container');
-    container.innerHTML = '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/KQetemT1sWc?autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="position: absolute; top: 0; left: 0;"></iframe>';
+    var iframe = document.createElement('iframe');
+
+    iframe.src = "https://www.youtube.com/embed/KQetemT1sWc?autoplay=1";
+    iframe.width = "100%";
+    iframe.height = "100%";
+    iframe.frameBorder = "0";
+    iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+    iframe.allowFullscreen = true;
+    iframe.style.position = "absolute";
+    iframe.style.top = "0";
+    iframe.style.left = "0";
+
+    iframe.onerror = function() {
+        window.location.href = "https://www.youtube.com/watch?v=KQetemT1sWc";
+    };
+
+    container.innerHTML = ''; 
+    container.appendChild(iframe); 
 }
