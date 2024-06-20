4a95a701f7f55f21a4e2b2dd8234195f1b50cc64
Alejandro Granero Nogales <agranerono@uoc.edu>
2024-06-20 02:04:25 +0200

    test video 2

--- src/scripts/video.js
+++ src/scripts/video.js
@@ -1,21 +1,20 @@
 function loadYoutubeVideo() {
     var container = document.querySelector('.youtube-container');
     var iframe = document.createElement('iframe');
-
-    iframe.src = "https://www.youtube.com/embed/KQetemT1sWc?autoplay=1";
-    iframe.width = "100%";
-    iframe.height = "100%";
-    iframe.frameBorder = "0";
-    iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-    iframe.allowFullscreen = true;
-    iframe.style.position = "absolute";
-    iframe.style.top = "0";
-    iframe.style.left = "0";
+    iframe.setAttribute('src', 'https://www.youtube.com/embed/KQetemT1sWc?autoplay=1');
+    iframe.setAttribute('frameborder', '0');
+    iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
+    iframe.setAttribute('allowfullscreen', 'true');
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.style.position = 'absolute';
+    iframe.style.top = '0';
+    iframe.style.left = '0';
 
     iframe.onerror = function() {
-        window.location.href = "https://www.youtube.com/watch?v=KQetemT1sWc";
+        window.open('https://www.youtube.com/watch?v=KQetemT1sWc&ab_channel=TheBeatlesVEVO', '_blank');
     };
 
-    container.innerHTML = ''; 
-    container.appendChild(iframe); 
+    container.innerHTML = '';
+    container.appendChild(iframe);
 }
