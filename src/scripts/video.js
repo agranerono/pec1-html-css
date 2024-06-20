35b146184ad938601789f3bbb42c7cde8c058338
Alejandro Granero Nogales <agranerono@uoc.edu>
2024-06-20 02:13:43 +0200

    test video 4

--- src/scripts/video.js
+++ src/scripts/video.js
@@ -1,20 +1,4 @@
 function loadYoutubeVideo() {
     var container = document.querySelector('.youtube-container');
-    var iframe = document.createElement('iframe');
-    iframe.setAttribute('src', 'https://www.youtube.com/embed/KQetemT1sWc?autoplay=1');
-    iframe.setAttribute('frameborder', '0');
-    iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
-    iframe.setAttribute('allowfullscreen', 'true');
-    iframe.style.width = '100%';
-    iframe.style.height = '100%';
-    iframe.style.position = 'absolute';
-    iframe.style.top = '0';
-    iframe.style.left = '0';
-
-    iframe.onerror = function() {
-        window.open('https://www.youtube.com/watch?v=KQetemT1sWc&ab_channel=TheBeatlesVEVO', '_blank');
-    };
-
-    container.innerHTML = '';
-    container.appendChild(iframe);
+    container.innerHTML = '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/KQetemT1sWc?autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="position: absolute; top: 0; left: 0;"></iframe>';
 }
